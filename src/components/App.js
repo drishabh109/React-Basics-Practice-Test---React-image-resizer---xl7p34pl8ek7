09bada2ac412fe949800338e045812b3ec3f9134
drishabh109 <96375523+drishabh109@users.noreply.github.com>
2022-04-01 22:53:29 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import image from "../image.jpeg";
 
 const App = () => {
   return (
     <div id="main">
-
+      <img src={image.jpeg} id="resizable-img" alt="" />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
